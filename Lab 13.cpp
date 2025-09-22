0535d972e2725a8a2fdfecea9f44593fd2b1e914
Tracy-115 <kmin984@insite.4cd.edu>
2025-09-21 18:16:54 -0700

    Create Lab 13.cpp

---
+++ Lab 13.cpp
@@ -0,0 +1,42 @@
+// COMSC-210 | Lab 13 | Kyawt Thinzar Min
+// IDE used: Visual Studio Code
+
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+int main(){
+array<int, 30>numbers{};//array of 30 integers
+ifstream infile("Array_File.txt");//reads the file
+if (!infile){
+  cerr << "Error" << endl;//error message for when the file is not found
+}
+
+for (int i =0; i<30 ; i++){
+  infile >> numbers[i];//reads the numbers from the file
+}
+infile.close();
+cout << "This is a gathered data for Grades of the 30 students in the school" << endl;
+cout << "With gathered numbers, we will find the Max, Min, and Mean numbers!" << endl;
+cout << "Max: " << *max_element(numbers.begin(), numbers.end()) << endl; //finds the max number
+cout << "Min: " << *min_element(numbers.begin(), numbers.end()) << endl; //finds the min number
+cout << "Sum: " << accumulate(numbers.begin(), numbers.end(), 0) << endl;//finds the sum of the numbers
+
+int num;
+cout << "What is the position of the value you wish to see from the list(1-30): ";
+cin >> num;
+cout << "In position " << num << " the number is " << numbers.at(num-1) << endl; //finds the number in the position the user inputs
+
+sort(numbers.begin(), numbers.end());
+cout << "Sorted list: "; //prints the sorted list
+for (double val : numbers) cout << val << " "; cout << endl;
+sort(numbers.rbegin(), numbers.rend());
+cout << "Reversed list: "; //prints the reversed list
+for (double val : numbers) cout << val << " ";
+cout << endl;
+
+return 0;
+}
